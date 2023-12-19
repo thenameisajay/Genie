b28120b4888dea5b2d26749b5e3db0fb096f6a19
apm30 <m.ajaypradeep@icloud.com>
2023-12-19 14:11:52 +0000

    Heading Placement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 import React from "react";
 import { ModeToggle } from "@/components/ui/theme-toggle";
+import HomePage from "@/components/home-page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen">
+    <main className="flex min-h-screen flex-col">
       <div
         className="relative justify-end items-end flex flex-col w-full h-full text-center top-4 right-4 "
         id="theme-toggle"
       >
         <ModeToggle />
+      </div>
+      <div className="">
+        <HomePage />
       </div>
     </main>
   );
--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -1,8 +1,10 @@
-export default function Home() {
+export default function HomePage() {
   return (
     <>
-      <h1>Genie</h1>
-      <h3>I know All , I see All</h3>
+      <div className="flex flex-col  items-center text-center h-screen w-screen  absolute top-36 ">
+        <h1>Genie</h1>
+        <h3>I know All , I see All</h3>
+      </div>
     </>
   );
 }
