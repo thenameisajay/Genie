242afa4954e0d3871d2e0d1fa9b4fb2597bb2dc3
apm30 <m.ajaypradeep@icloud.com>
2023-12-19 15:09:22 +0000

    Mobile Placing for input field is done

--- src/components/ui/home/home-page.tsx
+++ src/components/ui/home/home-page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -8,10 +8,15 @@ import { ArrowCircleUp, ArrowBendDownLeft } from "@phosphor-icons/react";
 export default function HomePage() {
   const [textValue, setTextValue] = useState<string>("");
 
+  const handleButtonClick = () => {
+    const fileInput = document.getElementById("picture");
+    fileInput?.click();
+  };
+
   return (
     <>
       <div className="flex flex-col  items-center text-center h-screen w-screen  absolute top-36 ">
-        <h1>Genie</h1>
+        <h1 className="">Genie</h1>
         <h3>I know All , I see All</h3>
         <div className=" relative top-20 w-auto h-auto ">
           <Card className="flex flex-col justify-center items-center h-32  w-screen ">
@@ -21,36 +26,35 @@ export default function HomePage() {
                   placeholder=" Ask me anything"
                   value={textValue}
                   onChange={(e) => setTextValue(e.target.value)}
-                  className=" w-72 h-16 px-2 "
+                  className=" w-72 h-16 px-2 relative  "
                 />
-                <div className="flex flex-row justify-center items-center mt-2">
+                <div className="flex flex-row justify-end items-end mt-2  w-64 mr-2">
                   <Button
-                    className="rounded-full p-2  border-b-orange-900"
+                    className="rounded-full p-2  w-10 h-10   bg-amber-900"
                     type="button"
-                    onClick={() => {
-                      console.log("upload");
-                    }}
+                    onClick={handleButtonClick}
                   >
-                    <ArrowCircleUp size={20} weight="fill" />
+                    <ArrowCircleUp size={25} weight="fill" />
                     <Input
                       id="picture"
                       type="file"
-                      className="mr-2 ml-2 hidden"
+                      className="mr-2 ml-2  "
+                      style={{ display: "none" }}
                     />
                   </Button>
                   <Button
                     type="submit"
                     disabled={textValue.length === 0}
-                    className="ml-2 rounded-full p-2"
+                    className="ml-2 rounded-full p-2  w-10 h-10 bg-black dark:bg-white"
                   >
-                    <ArrowBendDownLeft size={20} weight="fill" />
+                    <ArrowBendDownLeft size={25} weight="fill" className="" />
                   </Button>
                 </div>
               </div>
             </form>
-            <p>{textValue}</p>
           </Card>
         </div>
+        <p>{textValue}</p>
       </div>
     </>
   );
