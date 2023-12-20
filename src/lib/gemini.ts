56539b1e9ee479ad74cc8d16bdb6cd49d69c285a
apm30 <m.ajaypradeep@icloud.com>
2023-12-20 15:10:32 +0000

    Rewriting and  testing env vars

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,36 +1,7 @@
-const { VertexAI } = require("@google-cloud/vertexai");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
 
-// Initialize Vertex with your Cloud project and location
-const vertex_ai = new VertexAI({
-  project: "pics-392621",
-  location: "us-central1",
-});
-const model = "gemini-pro-vision";
+// Access your API key as an environment variable
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
 
-// Instantiate the models
-const generativeModel = vertex_ai.preview.getGenerativeModel({
-  model: model,
-  generation_config: {
-    max_output_tokens: 2048,
-    temperature: 0.4,
-    top_p: 1,
-    top_k: 32,
-  },
-});
-
-export async function generateContent(message: Array<Object>) {
-  const req = {
-    // This is where you specify the input text or the image in terms of objects
-    contents: [{ role: "user", parts: message }],
-  };
-
-  const streamingResp = await generativeModel.generateContentStream(req);
-
-  for await (const item of streamingResp.stream) {
-    process.stdout.write("stream chunk: " + item);
-  }
-
-  process.stdout.write(
-    "aggregated response: " + (await streamingResp.response)
-  );
-}
+const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
