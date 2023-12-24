bdb75fdc4a27dc20d846bb319b8165051bca7b40
apm30 <m.ajaypradeep@icloud.com>
2023-12-24 15:02:23 +0000

    upadating changes to tailwind 3.4

--- src/components/ui/home/home-page.tsx
+++ src/components/ui/home/home-page.tsx
@@ -13,13 +13,11 @@ export default function HomePage() {
   const handleButtonClick = () => {
     const fileInput = document.getElementById("picture");
     fileInput?.click();
-    const geminiTalk = run();
-    console.log(geminiTalk);
   };
 
   return (
     <>
-      <div className="flex flex-col  items-center text-center h-screen w-screen  absolute top-36 ">
+      <div className="flex flex-col  items-center text-center h-dvh w-dvw absolute top-36 ">
         <div className=" flex flex-col justify-center items-center">
           <Lamp size={70} weight="fill" className="mb-5" />
           <h1 className="text-7xl text-center flex items-center justify-center font-extrabold tracking-tight lg:text-9xl">
@@ -30,17 +28,17 @@ export default function HomePage() {
           </p>
         </div>
         <div className=" relative top-10 w-auto h-auto">
-          <Card className="flex flex-col justify-center items-center h-auto w-screen ">
+          <Card className="flex flex-col justify-center items-center h-auto px-2">
             <form>
               <div className="flex flex-col  justify-center items-center  w-auto ">
                 <Textarea
                   placeholder=" Ask anything"
                   value={textValue}
                   onChange={(e) => setTextValue(e.target.value)}
-                  className="  h-16 relative placeholder:text-base placeholder:font-semibold border-none top-2 mb-2 focus:border-none  "
+                  className="  h-16 relative placeholder:text-base placeholder:font-semibold border-none top-2 mb-5 focus:border-none w-full "
                 />
 
-                <div className="flex flex-row justify-end items-end mt-2  w-64 mr-2">
+                <div className="flex flex-row justify-end items-end mt-2  w-64 mr-2 mb-3">
                   <Button
                     className="rounded-full p-2  w-10 h-10   bg-amber-900"
                     type="button"
