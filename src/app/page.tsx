750d15c400c9db1e92abd713040d1db2c6098dcb
apm30 <m.ajaypradeep@icloud.com>
2023-12-25 21:42:10 +0000

    save button to main file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
 import React from "react";
 import { ModeToggle } from "@/components/ui/theme-toggle";
+import { Configuration } from "@/components/config-provider";
 import HomePage from "@/components/ui/home/home-page";
 
 export default function Home() {
   return (
     <main className="flex min-h-dvh w-dvw  flex-col">
       <div
-        className="relative justify-end items-end flex flex-col w-full h-full text-center top-4 right-4 "
+        className="relative justify-end items-end flex flex-row w-full h-full text-center top-4 right-4 "
         id="theme-toggle"
       >
+        <Configuration />
         <ModeToggle />
       </div>
       <HomePage />
--- src/components/config-provider.tsx
+++ src/components/config-provider.tsx
@@ -10,38 +10,45 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Sliders, GearSix } from "@phosphor-icons/react";
 
-export function DialogDemo() {
+export function Configuration() {
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button variant="outline">Edit Profile</Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you&aposre done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+    <div className="relative mr-4 top-1">
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button variant="outline" className="h-10">
+            {/* <Sliders size={32} weight="fill" /> */}
+            <GearSix size={36} color="#22b995" weight="duotone" />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&aposre
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <Input id="name" value="Pedro Duarte" className="col-span-3" />
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <Input id="username" value="@peduarte" className="col-span-3" />
+            </div>
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 }
