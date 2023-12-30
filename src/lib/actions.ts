66c4a869713f12308760de0492bf956cef37f825
apm30 <m.ajaypradeep@icloud.com>
2023-12-30 01:27:01 +0000

    red code

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -13,18 +13,3 @@ export async function fileToGenerativePart(file: File) {
     inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
   };
 }
-
-/**
- *
- * @param image The image to convert to base64
- * @returns  The base64 string of the image.
- *
- */
-export async function imageToBase64(image: File) {
-  return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(image);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-  });
-}
