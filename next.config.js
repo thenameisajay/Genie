497dcec75b05393e3773d8aa5b2ce3c968001ecc
apm30 <m.ajaypradeep@icloud.com>
2024-01-14 22:48:41 +0000

    remove console.logs before hitting prod & add new ss working in uk

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    compiler: {
+        removeConsole:
+            process.env.NODE_ENV === "production" ? { exclude: ["error"] } : false,
+    },
+}
 
 module.exports = nextConfig
