1336dbd3d427df6f07ae489eb09a9b0374ec1190
apm30 <m.ajaypradeep@icloud.com>
2024-04-01 17:45:09 +0100

    new refactor in error component

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -32,11 +32,6 @@ export default function HomeComponent() {
     const [maxToken, setMaxToken] = useState<number>(0);
     const [generationConfig, setGenerationConfig] = useState<object>({});
 
-    const handleButtonClick = () => {
-        const fileInput = document.getElementById('picture');
-        fileInput?.click();
-    };
-
     const getValues = () => {
         if (localStorage !== undefined) {
             // Check for configuration settings
@@ -53,91 +48,6 @@ export default function HomeComponent() {
             } else {
                 setMaxToken(0);
             }
-        }
-    };
-
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        setResponse('');
-        event.preventDefault();
-        setIsLoading(true);
-        getValues();
-
-        console.log('maxToken', maxToken);
-
-        if (maxToken > 0) {
-            setGenerationConfig({
-                maxNumTokens: maxToken,
-            });
-        }
-
-        // Packing text and images into an object called message and send it to the api
-        try {
-            if (imageParts === undefined || imageParts.length == 0) {
-                const message = {
-                    text: textValue.trim(),
-                };
-
-                void run(message, apikeys, generationConfig)
-                    .then((response) => {
-                        setResponse(response as string);
-                        setIsLoading(false);
-
-                        setImageParts([]);
-                        setTextValue('');
-                    })
-                    .catch((error) => {
-                        setIsLoading(false);
-                        setIsError(true);
-                        const { message } = error;
-                        setErrorMessage(message as string);
-                        throw new Error(error as string);
-                    });
-            } else {
-                // TODO : Handle states for image  generaation model
-                const message = {
-                    text: textValue.trim(),
-                    imageParts: imageParts,
-                };
-
-                // Take the message object and send it to the api
-                void run(message, apikeys, generationConfig)
-                    .then((response) => {
-                        if (response.length > 0 && response !== undefined) {
-                            setResponse(response as string);
-                            setIsLoading(false);
-                            setImageParts([]);
-                            setTextValue('');
-                        }
-                    })
-                    .catch((error) => {
-                        throw new Error(error as string);
-                    });
-            }
-        } catch (error) {
-            setIsLoading(false);
-            setIsError(true);
-            setErrorMessage(error as string);
-            console.error(error);
-        }
-    };
-
-    const handleImages = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-
-        if (files && files.length > 0) {
-            toast.success(` ${files.length} Files selected`);
-
-            // Convert to generative parts and store in an array of objects
-
-            const generativePart: Array<object> = [];
-            for (let i = 0; i < files.length; i++) {
-                void fileToGenerativePart(files[i]).then((base64Image) => {
-                    generativePart.push(base64Image);
-                    setImageParts(generativePart);
-                });
-            }
-        } else {
-            toast.error('No files selected');
         }
     };
 
@@ -171,6 +81,95 @@ export default function HomeComponent() {
     };
 
     const MiddleComponent = () => {
+        const handleButtonClick = () => {
+            const fileInput = document.getElementById('picture');
+            fileInput?.click();
+        };
+        const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+            setResponse('');
+            event.preventDefault();
+            setIsLoading(true);
+            getValues();
+
+            console.log('maxToken', maxToken);
+
+            if (maxToken > 0) {
+                setGenerationConfig({
+                    maxNumTokens: maxToken,
+                });
+            }
+
+            // Packing text and images into an object called message and send it to the api
+            try {
+                if (imageParts === undefined || imageParts.length == 0) {
+                    const message = {
+                        text: textValue.trim(),
+                    };
+
+                    void run(message, apikeys, generationConfig)
+                        .then((response) => {
+                            setResponse(response as string);
+                            setIsLoading(false);
+
+                            setImageParts([]);
+                            setTextValue('');
+                        })
+                        .catch((error) => {
+                            setIsLoading(false);
+                            setIsError(true);
+                            const { message } = error;
+                            setErrorMessage(message as string);
+                            throw new Error(error as string);
+                        });
+                } else {
+                    // TODO : Handle states for image  generaation model
+                    const message = {
+                        text: textValue.trim(),
+                        imageParts: imageParts,
+                    };
+
+                    // Take the message object and send it to the api
+                    void run(message, apikeys, generationConfig)
+                        .then((response) => {
+                            if (response.length > 0 && response !== undefined) {
+                                setResponse(response as string);
+                                setIsLoading(false);
+                                setImageParts([]);
+                                setTextValue('');
+                            }
+                        })
+                        .catch((error) => {
+                            throw new Error(error as string);
+                        });
+                }
+            } catch (error) {
+                setIsLoading(false);
+                setIsError(true);
+                setErrorMessage(error as string);
+                console.error(error);
+            }
+        };
+
+        const handleImages = (event: React.ChangeEvent<HTMLInputElement>) => {
+            const files = event.target.files;
+
+            if (files && files.length > 0) {
+                toast.success(` ${files.length} Files selected`);
+
+                // Convert to generative parts and store in an array of objects
+
+                const generativePart: Array<object> = [];
+                for (let i = 0; i < files.length; i++) {
+                    void fileToGenerativePart(files[i]).then((base64Image) => {
+                        generativePart.push(base64Image);
+                        setImageParts(generativePart);
+                    });
+                }
+            } else {
+                toast.error('No files selected');
+            }
+        };
+
         return (
             <div className=" relative top-10 h-auto w-auto">
                 <Card className="flex h-auto flex-col items-center justify-center px-2  sm:w-96">
@@ -182,7 +181,6 @@ export default function HomeComponent() {
                                 onChange={(e) => setTextValue(e.target.value)}
                                 className="  relative top-2 mb-5 h-16 w-full border-none placeholder:text-base placeholder:font-semibold focus:border-none "
                             />
-
                             <div className="mb-3 mr-2 mt-2 flex w-64  flex-row items-end justify-end">
                                 <Button
                                     className="h-10 w-10  rounded-full bg-amber-900   p-2"
